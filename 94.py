09e844cb3b42b0e8beaed77548806388305115da
Meer Husamuddin <meerhusamm@gmail.com>
2023-12-07 22:10:30 -0500

    Solution to Q94

---
+++ 94.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        output = []
+        self = root
+
+        def dfs(self):
+            if self.left != None:
+                dfs(self.left)
+            output.append(self.val)
+            if self.right != None:
+                dfs(self.right)
+            return
+
+        if(self != None):
+            dfs(self)
+            
+        return(output)
+        
+        
